5d9b6463661e5a451e8871d2d5eef26833995f9e
LeeYoungIn <pimery@nate.com>
2015-05-30 11:30:36 +0900

    New changelist

--- dodream/settings.py
+++ dodream/settings.py
@@ -144,12 +144,6 @@ CKEDITOR_CONFIGS = {
     },
 }
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-    'apptemplates.Loader',
-)
-
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
 
